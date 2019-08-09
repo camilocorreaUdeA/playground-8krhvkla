a9d0a49bd28e43af847f210e481308c8d73e3794
camilocorreaUdeA <42076547+camilocorreaUdeA@users.noreply.github.com>
2019-08-09 14:08:13 -0500

    Update constructores.h

--- cpp-project/src/Constructores/constructores.h
+++ cpp-project/src/Constructores/constructores.h
@@ -10,6 +10,7 @@ class Constructores
 	
 	public:
 	/*Implemente los constructores de esta clase */
+	Constructores()=default;
 	Constructores(int x, int y, int z){}
 	void printInfo(){ cout<<"int a = "<<a<<" double b = "<<b<<" string c = "<<c<<endl; }	
 };
