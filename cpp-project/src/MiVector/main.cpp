758a52318c070c826404fb1dcee91433f57f6447
camilocorreaUdeA <42076547+camilocorreaUdeA@users.noreply.github.com>
2019-08-09 14:29:28 -0500

    Update main.cpp

--- cpp-project/src/MiVector/main.cpp
+++ cpp-project/src/MiVector/main.cpp
@@ -13,12 +13,12 @@ int main()
 	//Suma:	
 	MiVector3D suma = vec1 + vec2;
 	cout<<"Suma: ";
-	cout<<suma.vec[0]<<","<<suma.vec[1]<<","<<suma.vec[2]<<endl;
+	cout<<"["<<suma.vec[0]<<","<<suma.vec[1]<<","<<suma.vec[2]<<"]"<<endl;
 	
 	//Resta:	
 	MiVector3D resta = vec2 - vec1;
 	cout<<"Resta: ";
-	cout<<resta.vec[0]<<","<<resta.vec[1]<<","<<resta.vec[2]<<endl;
+	cout<<"["<<resta.vec[0]<<","<<resta.vec[1]<<","<<resta.vec[2]<<"]"<<endl;
 	
 	//Producto punto:	
 	double prodpun = vec2 * vec1;
@@ -27,12 +27,12 @@ int main()
 	//Division por un escalar
 	MiVector3D division = vec2/10;
 	cout<<"Division: ";
-	cout<<division.vec[0]<<","<<division.vec[1]<<","<<division.vec[2]<<endl;
+	cout<<"["<<division.vec[0]<<","<<division.vec[1]<<","<<division.vec[2]<<"]"<<endl;
 	
 	//Potenciacion
 	MiVector3D potencia = vec1>>2;
 	cout<<"Potenciacion: ";
-	cout<<potencia.vec[0]<<","<<potencia.vec[1]<<","<<potencia.vec[2]<<endl;	
+	cout<<"["<<potencia.vec[0]<<","<<potencia.vec[1]<<","<<potencia.vec[2]<<"]"<<endl;	
 	
 	return 0;	
 }
